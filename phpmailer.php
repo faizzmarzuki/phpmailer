c2a4897cd11790a4a1fe384f0938f936649e8631
Mohammad Faiz Bin Marzuki <faizz.marzukii@gmail.com>
2023-06-24 01:05:17 +0800

    added comment

--- phpmailer.php
+++ phpmailer.php
@@ -2,42 +2,46 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+//initialize phpmailer location
 require './PHPMailer/PHPMailer/src/PHPMailer.php';
 require './PHPMailer/PHPMailer/src/SMTP.php';
 require './PHPMailer/PHPMailer/src/Exception.php';
 
-if (isset($_POST['btnSubmit'])) {
-  $email = $_POST['email'];
-  $feedback = $_POST['feedback'];
+//phpmailer condition
+if (isset($_POST['btnSubmit'])) { //which button work as submit button, initialize button name
+  $email = $_POST['email']; //initialize variable with input name in html
+  $feedback = $_POST['feedback']; //initialize variable with input name in html
 
-  $mail = new PHPMailer(true);
+  $mail = new PHPMailer(true); //define  mail
 
   try {
     // SMTP Configuration
     $mail->isSMTP();
-    $mail->Host = '	smtp.gmail.com';
+    $mail->Host = '	smtp.gmail.com';//host dont change anything here
     $mail->SMTPAuth = true;
-    $mail->Username = '';
-    $mail->Password = '';
+    $mail->Username = '';//insert sender email (real email!!)
+    $mail->Password = '';//insert sender password (real email!!)
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
-    $mail->setFrom('', 'Your Name');
+    $mail->Port = 587; //dont change port
+    $mail->setFrom('', 'Your Name');//you can use dummy email here
 
     // Recipient
-    $mail->addAddress($email, 'Recipient Name');
+    $mail->addAddress($email, 'Recipient Name');//receiver
 
     // Email content
     $mail->isHTML(true);
-    $mail->Subject = 'New Contact Form Submission';
-    $mail->Body = "Email: $email\nFeedback: $feedback";
+    $mail->Subject = 'New Contact Form Submission';//email title
+    $mail->Body = "Email: $email\nFeedback: $feedback";//email content
 
     // Send the email
     $mail->send();
+    //echo below will give an alert to show that the email has been send
     echo "<script> 
       alert('Sent Successfully')
       document.location.href = 'index.html';
     </script>";
   } catch (Exception $e) {
+    //will display error
     echo "Error sending email: {$mail->ErrorInfo}";
   }
 }
