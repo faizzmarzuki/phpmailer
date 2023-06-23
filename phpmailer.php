e6e64b150bd6d1c2f32ee60e435211feb433748f
Mohammad Faiz Bin Marzuki <faizz.marzukii@gmail.com>
2023-06-24 00:56:10 +0800

    initialize php mailer

---
+++ phpmailer.php
@@ -0,0 +1,43 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './PHPMailer/PHPMailer/src/PHPMailer.php';
+require './PHPMailer/PHPMailer/src/SMTP.php';
+require './PHPMailer/PHPMailer/src/Exception.php';
+
+if (isset($_POST['btnSubmit'])) {
+  $email = $_POST['email'];
+  $feedback = $_POST['feedback'];
+
+  $mail = new PHPMailer(true);
+
+  try {
+    // SMTP Configuration
+    $mail->isSMTP();
+    $mail->Host = '	smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'faizz.marzukii@gmail.com';
+    $mail->Password = 'vvluglkpqepvgoce';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+    $mail->setFrom('testmail@gmail.com', 'Your Name');
+
+    // Recipient
+    $mail->addAddress($email, 'Recipient Name');
+
+    // Email content
+    $mail->isHTML(true);
+    $mail->Subject = 'New Contact Form Submission';
+    $mail->Body = "Email: $email\nFeedback: $feedback";
+
+    // Send the email
+    $mail->send();
+    echo "<script> 
+      alert('Sent Successfully')
+      document.location.href = 'index.html';
+    </script>";
+  } catch (Exception $e) {
+    echo "Error sending email: {$mail->ErrorInfo}";
+  }
+}
